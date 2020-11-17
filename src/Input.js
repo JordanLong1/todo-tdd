4dd19e0fe1dacd69f8c45113196f2152540d8711
Jordan <jordanleelong5@gmail.com>
2020-11-17 12:49:48 -0800

    added handleChange and passed tests

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,31 @@
 import React from 'react'; 
+import ListToDos from './ListToDos'
 
 class Input extends React.Component {
+    state = {inputText: ''}
+
+handleChange = (event) => {
+    this.setState({inputText: event.target.value})
+}
+
+handleSubmit = (e) => {
+    e.preventDefault(); 
+    
+}
+
     render() {
         return (
-            <div>
-
+            <div data-test='input-component'>
+                <form className='input-form' onSubmit={this.handleSubmit}>
+            <input className='input'
+            data-test='input-field'
+            type='text'
+            onChange={this.handleChange}
+            value={this.state.inputText}
+            />
+            <button data-test='submit-button'>Submit</button>
+                </form>
+                <ListToDos />
             </div>
         )
     };
