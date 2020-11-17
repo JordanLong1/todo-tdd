7efdcae21fc1c8e34539bd00e5dfe275af770a98
Jordan <jordanleelong5@gmail.com>
2020-11-17 12:50:25 -0800

    added boilerplate for functional component to list props

---
+++ src/ListToDos.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const ListToDos = props => {
+    return (
+        <div className='todos'>
+            Hello
+        </div >
+    );
+};
+
+export default ListToDos
