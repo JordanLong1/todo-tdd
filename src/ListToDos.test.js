fe5558421630abb706585bf30ab29f80e84cc8ac
Jordan <jordanleelong5@gmail.com>
2020-11-17 22:00:49 -0800

    added tests that need to be completed

--- src/ListToDos.test.js
+++ src/ListToDos.test.js
@@ -1,3 +1,10 @@
-test('skeleton of test', () => {
 
-})
+test('renders component without throwing error', () => {
+
+}); 
+test('on form submission, props have a length of one', () => {
+
+}); 
+test('rendes li with todo data without throwing an error', () => {
+
+}); 
