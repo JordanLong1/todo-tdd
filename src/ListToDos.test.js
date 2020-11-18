8cb9a197f198b31125d81584bd5769b6badacab8
Jordan <jordanleelong5@gmail.com>
2020-11-17 22:53:16 -0800

    wrote first three tests

--- src/ListToDos.test.js
+++ src/ListToDos.test.js
@@ -1,10 +1,23 @@
+import {shallow} from 'enzyme';
+import ListToDos from './ListToDos';
+const setUp = () => shallow(<ListToDos />);
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
 test('renders component without throwing error', () => {
-
+    const wrapper = setUp(); 
+    const component = findByTestAttr(wrapper, 'todos-component'); 
+    expect(component.length).toBe(1); 
 }); 
-test('on form submission, props have a length of one', () => {
-
+test('on Input form submission, props have a length of one', () => {
+    const wrapper = setUp(); 
+    expect(wrapper.props().length).toBe(1);
 }); 
 test('rendes li with todo data without throwing an error', () => {
-
+    const wrapper = setUp(); 
+    const list = findByTestAttr(wrapper, 'todos-li'); 
+    expect(list.length).toBe(1); 
 }); 
+
+test('li contains data from props', () => {
+
+});
